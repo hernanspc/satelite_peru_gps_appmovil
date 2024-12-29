e240272d9c1e25c24fbb1841c148cfa21097cae2
Hernan <zoluxiones38@banbif.com.pe>
2024-12-29 16:38:28 -0500

    feat: router tsx

--- lib/data/config/router/router.dart
+++ lib/data/config/router/router.dart
@@ -20,21 +20,6 @@ final appRouter = GoRouter(
       path: '/home_screen',
       name: NavigationHomeScreen.nameScreen,
       builder: (context, state) => NavigationHomeScreen(),
-      // pageBuilder: (context, state) {
-      //   return CustomTransitionPage(
-      //     // transitionDuration: Duration(seconds: 1),
-      //     fullscreenDialog: true,
-      //     key: state.pageKey,
-      //     child: HomeScreen(),
-      //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      //       return FadeTransition(
-      //         opacity: animation,
-      //         // opacity: CurveTween(curve: Curves.linear).animate(animation),
-      //         child: child,
-      //       );
-      //     },
-      //   );
-      // },
     ),
     GoRoute(
       path: '/bus_map_view_screen',
@@ -48,17 +33,5 @@ final appRouter = GoRouter(
       path: '/bus_map_historial',
       builder: (context, state) => BusMapHistorial(),
     )
-    // GoRoute(
-    //   path: '/register_person',
-    //   builder: (context, state) {
-    //     final filter1 = state.uri.queryParameters['filter1'];
-    //     final filter2 = state.uri.queryParameters['filter2'];
-
-    //     return RegisterPerson(
-    //       filter1: filter1 ?? '',
-    //       filter2: filter2 ?? '',
-    //     );
-    //   },
-    // ),
   ],
 );
